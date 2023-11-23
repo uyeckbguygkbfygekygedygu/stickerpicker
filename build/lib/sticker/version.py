e166d7ceeac3cdd4383b23c309b9a85f171e524c
Manav Gunnia Madan <manavgunnia@gmail.com>
2023-11-23 23:46:28 +1100

    Revert "Add animated stickers as a test."
    This reverts commit ca302d0ac67f4775ac83e6becd64715043caf0ed.
    
    Test failed.

--- build/lib/sticker/version.py
+++
@@ -1,6 +0,0 @@
-# Generated in setup.py
-
-git_tag = None
-git_revision = 'unknown'
-version = '0.1.0+dev.unknown'
-linkified_version = '0.1.0+dev.unknown'
--- sticker/version.py
+++ sticker/version.py
@@ -1,6 +1,1 @@
-# Generated in setup.py
-
-git_tag = None
-git_revision = 'unknown'
-version = '0.1.0+dev.unknown'
-linkified_version = '0.1.0+dev.unknown'
+from .get_version import git_tag, git_revision, version, linkified_version
